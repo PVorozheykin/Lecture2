fd15d7a9229ce5907556df85a89f813ef9b705a6
PVorozheykin <vorozheykin@mer.ci.nsu.ru>
2023-09-11 12:00:27 +0700

    Add logging

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace App
+﻿using Serilog;
+
+
+namespace App;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .WriteTo.Console()
+            .WriteTo.File("log/log.log")
+            .CreateLogger();
+
+        Log.Debug("Application start");
+
+        // Run
+
+
+
+        Log.Debug("Application stop");
+        Log.CloseAndFlush();
     }
 }
